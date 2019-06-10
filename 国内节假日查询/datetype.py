27da5db8741642501b4610b40f3a1cea4766e4b4
Mr-watermelon1 <37217739+Mr-watermelon1@users.noreply.github.com>
2019-06-10 14:41:27 +0800

    Update datetype.py

--- 国内节假日查询/datetype.py
+++ 国内节假日查询/datetype.py
@@ -18,19 +18,21 @@ def goseek(start_date,end_date):
         date = datestart.strftime('%Y%m%d')
         vop_response = request.urlopen(server_url + date)
         vop_data= json.loads(vop_response.read().decode('utf-8'))
-        # 0代表工作日，1代表周末，2代表节假日
+        # 0代表工作日，1代表节假日，2代表调休补班，3代表休息日
         if vop_data['data'] == 0:
             print(date,vop_data['data'],'工作日')
         elif vop_data['data'] == 1:
-            print(date,vop_data['data'],'周末')
-        elif vop_data['data'] == 2:
             print(date,vop_data['data'],'节假日')
+        elif vop_data['data'] == 2:
+            print(date,vop_data['data'],'调休补班')
+        elif vop_data['data'] == 3:
+            print(date,vop_data['data'],'休息日')
         else:
             print('Error')
     print("process is over")
     print("run number is %s" % num)
 if __name__ == '__main__':
     start_time = time.time()
-    goseek('2016-12-31','2019-12-31')
+    goseek('2019-06-01','2019-12-31')
     stop_time = time.time()
     print('run time is %s' % (stop_time - start_time))
